2fef527040734f91e68b7613b5d8272f43e0a63c
Gustavo Perdomo <gperdomor@gmail.com>
2021-10-07 11:15:18 -0300

    feat(nx-docusaurus): build executor implementation

--- packages/nx-docusaurus/src/executors/build/executor.ts
+++ packages/nx-docusaurus/src/executors/build/executor.ts
@@ -1,8 +1,25 @@
-import { BuildExecutorSchema } from './schema';
+import { build } from '@docusaurus/core/lib';
+import { ExecutorContext, joinPathFragments } from '@nrwl/devkit';
+import { info, startGroup } from '@nx-tools/core';
+import { DocusaurusBuildSchema } from './schema';
 
-export default async function runExecutor(options: BuildExecutorSchema) {
-  console.log('Executor ran for Build', options);
-  return {
-    success: true,
-  };
+export default async function runExecutor(
+  options: DocusaurusBuildSchema,
+  context?: ExecutorContext,
+): Promise<{ success: true }> {
+  startGroup('Building docs...', 'Nx Docusaurus');
+
+  info('options --> ' + JSON.stringify(options, null, 2));
+
+  info('context --> ' + JSON.stringify(context, null, 2));
+
+  const projectRoot = context?.workspace.projects[context.projectName].root;
+
+  await build(projectRoot, {
+    bundleAnalyzer: options.bundleAnalyzer,
+    outDir: joinPathFragments(context.root, options.outputPath),
+    minify: options.minify,
+  });
+
+  return { success: true };
 }
